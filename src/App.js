a4fac62483dce23d8e3965daa957f5504c6a1be2
Mehdi Jarraya <mehdi.jarraya@gmail.com>
2022-05-10 03:37:37 +0100

    add spinner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import CustomTable from "./components/CustomTable"
 import { employeeColumns } from "./constants"
+import Spinner from 'react-bootstrap/Spinner'
 import './App.css';
 
 function App() {
@@ -10,16 +11,18 @@ function App() {
   // const backendUrl = process.env.BACKEND_URL 
   const backendUrl = 'http://localhost:3001'
 
+  const [loading, setLoading] = useState(false)
   const [employees, setEmployees] = useState([])
   const [searchedName, setSearchedName] = useState("")
 
   const searchHandler = async () => {
+    setLoading(true)
     const url = searchedName ? `${backendUrl}/employees/search/${searchedName}` : `${backendUrl}/employees/all`
     const result = await axios.get(url)
-    console.log("result", result);
     if (result?.data) {
       setEmployees(result?.data)
     }
+    setLoading(false)
   }
 
   const handleInputChange = (e) => {
@@ -27,14 +30,30 @@ function App() {
     setSearchedName(e.target.value)
   }
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchHandler()
+    }
+  }
+
   useEffect(() => {
     (async () => {
+      setLoading(true)
       const result = await axios.get(`${backendUrl}/employees/all`)
       if (result?.data) {
         setEmployees(result?.data)
       }
+      setLoading(false)
     })()
   }, []);
+
+  if (loading) {
+    return <Container className="App">
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </Container>
+  }
 
   return (
     <Container className="App">
@@ -45,15 +64,13 @@ function App() {
           aria-describedby="basic-addon2"
           onChange={handleInputChange}
           value={searchedName}
-
+          onKeyDown={handleKeyDown}
         />
         <Button variant="primary" id="button-addon2"
           onClick={searchHandler}>
           Search
         </Button>
       </InputGroup>
-
-
       {employees.length > 0 ? <CustomTable data={employees} columns={employeeColumns} limit={5} /> : <h3>No employees found</h3>}
     </Container>
 
